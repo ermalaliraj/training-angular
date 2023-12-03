e0d29038bfa875fc1b957685c2aa9600a3b441c8
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2023-12-03 19:53:55 +0100

    all

--- my-prj/src/app/app.module.ts
+++ my-prj/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -17,10 +20,13 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     RecipeListComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgForOf
   ],
   providers: [],
   bootstrap: [AppComponent]
